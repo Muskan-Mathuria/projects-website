1033a42e38bc20400c83e4bc75a7d1f7de22c78e
mmathuria_707121 <muskanmathuria09@gmail.com>
2025-12-23 20:18:58 +0530

    Create Code2.java

---
+++ JAVA/Code2.java
@@ -0,0 +1,16 @@
+public class code2 {
+    public static void main(String[] args) {
+//        DATA TYPES
+        int a=10;
+        String b="JAVA";
+        double c=3.14;
+        boolean d=true;
+        char f='a';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+
+    }
+}
